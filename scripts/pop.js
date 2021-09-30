d58ad8a65bfb2c171da8960a27786865735e3956
L3ungj <justin.leungj@gmail.com>
2021-09-30 22:53:00 +0800

    testing

---
+++ scripts/pop.js
@@ -0,0 +1,7 @@
+// Add event listener on keydown
+document.addEventListener('keydown', (event) => {
+    var name = event.key;
+    var code = event.code;
+    // Alert the key name and key code on keydown
+    alert(`Key pressed ${name} \r\n Key code value: ${code}`);
+  }, false);
