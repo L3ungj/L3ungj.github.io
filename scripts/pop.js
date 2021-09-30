53edba4e4d3052924e9f44d8e9991ac06b7ff274
L3ungj <justin.leungj@gmail.com>
2021-10-01 00:23:44 +0800

    ver 1

--- scripts/pop.js
+++ scripts/pop.js
@@ -1,10 +1,5 @@
-const main_element = document.getElementsByClassName("main");
-const normal_img_name = "ak.png"
-const popped_img_name = "popak.png"
-
-function addClick() {
-    main_element.lastChild.setAttribute("src", "/assets/" + popped_img_name);
-}
+const normal_img_name = "ak.png";
+const popped_img_name = "popak.png";
 
 document.addEventListener('keydown', (event) => {
     var name = event.key;
@@ -17,5 +12,25 @@ document.addEventListener('keyup', (event) => {
     var name = event.key;
     var code = event.code;
     console.log(`Key pressed ${name} \r\n Key code value: ${code}`)
-    main_element.lastChild.setAttribute("src", "/assets/" + normal_img_name);
+    revert();
 }, false);
+
+document.addEventListener('mousedown', event => {
+    addClick();
+}, false);
+
+document.addEventListener('mouseup', (event) => {
+    revert();
+}, false);
+
+function addClick() {
+    // console.log(main_element.children);
+    document.getElementById("image").setAttribute("src", "/assets/" + popped_img_name);
+    document.getElementById("counter").innerHTML = parseInt(document.getElementById("counter").innerHTML) + 1
+};
+
+function revert()
+{
+    var img_element = document.getElementById("image");
+    img_element.setAttribute("src", "/assets/" + normal_img_name);
+};
