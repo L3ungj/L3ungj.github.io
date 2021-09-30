b023ba8a7e45d25e4ccaf85339cb13d8908cc7ad
L3ungj <justin.leungj@gmail.com>
2021-09-30 23:18:20 +0800

    pls work

--- scripts/pop.js
+++ scripts/pop.js
@@ -1,7 +1,21 @@
-// Add event listener on keydown
+const main_element = document.getElementsByClassName("main");
+const normal_img_name = "ak.png"
+const popped_img_name = "popak.png"
+
+function addClick() {
+    main_element.lastChild.setAttribute("src", "/assets/" + popped_img_name);
+}
+
 document.addEventListener('keydown', (event) => {
     var name = event.key;
     var code = event.code;
-    // Alert the key name and key code on keydown
-    alert(`Key pressed ${name} \r\n Key code value: ${code}`);
-  }, false);
+    console.log(`Key pressed ${name} \r\n Key code value: ${code}`)
+    addClick()
+}, false);
+
+document.addEventListener('keyup', (event) => {
+    var name = event.key;
+    var code = event.code;
+    console.log(`Key pressed ${name} \r\n Key code value: ${code}`)
+    main_element.lastChild.setAttribute("src", "/assets/" + normal_img_name);
+}, false);
